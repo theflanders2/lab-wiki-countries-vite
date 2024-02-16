b6e4f3f0afb0e0b9e33d7d2a7f5acf5e19a5aa08
theflanders2 <kenneth.flanders.ii@gmail.com>
2024-02-16 19:21:04 +0100

    add packages antd and axios, iteration 3 completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,30 @@
-function HomePage() {
-    return (
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Spin } from 'antd';
 
+const countriesApi = "https://ih-countries-api.herokuapp.com/countries"; // countries data from API stored in a variable
+
+function HomePage() {
+  const [fetching, setFetching] = useState(true); // activate spin animation
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect - Initial render (Mounting)");
+    console.log("Fetching data...");
+    axios.get(countriesApi) // this will fetch the countries data from the api
+      .then((response) => {
+        console.log("API response data", response.data)
+        setCountries(response.data); // add the list of countries to the state
+        setFetching(false); // remove the spin animation
+      });
+  }, []); // useEffect will only be triggered one time since the array is empty
+
+    return (
         <div className="container" style={{maxHeight: "90vh overflow scroll"}}>
         <h1 style={{fontSize: "24px"}}>WikiCountries: Your Guide to the World</h1>
 
         <div className="list-group">
-          <a className="list-group-item list-group-item-action" href="/ABW"> 🇦🇼 Aruba </a>
+          {/* <a className="list-group-item list-group-item-action" href="/ABW"> 🇦🇼 Aruba </a>
           <a className="list-group-item list-group-item-action" href="/AFG"> 🇦🇫 Afghanistan </a>
           <a className="list-group-item list-group-item-action" href="/AGO"> 🇦🇴 Angola </a>
           <a className="list-group-item list-group-item-action" href="/AIA"> 🇦🇮 Anguilla </a>
@@ -19,7 +38,14 @@ function HomePage() {
           <a className="list-group-item list-group-item-action" href="/ATA"> 🇦🇶 Antarctica </a>
           <a className="list-group-item list-group-item-action" href="/FLK"> 🇫🇰 Falkland Islands </a>
           <a className="list-group-item list-group-item-action active" href="/FRA"> 🇫🇷 France </a>
-          <a className="list-group-item list-group-item-action" href="/ZWE"> 🇿🇼 Zimbabwe </a>
+          <a className="list-group-item list-group-item-action" href="/ZWE"> 🇿🇼 Zimbabwe </a> */}
+          {fetching &&  <Spin />}
+
+          {countries.map((country) => {
+        return (
+          <p className="list-group-item list-group-item-action" key={country._id}>{country.name.common}</p>
+        )
+      })}
         </div>
       </div>
     );
